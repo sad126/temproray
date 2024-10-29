adf70b20fcecd783dc0a14d3bb1077ce929b76b4
sad126 <frhatsd@gmail.com>
2024-10-29 15:29:19 +0300

    1111

--- Assets/scripts/Temp.cs
+++ Assets/scripts/Temp.cs
@@ -7,7 +7,7 @@ public class Temp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("dfsvchjnx");
     }
 
     // Update is called once per frame

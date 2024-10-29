d2a53eaeb88613d7cc2a2727bbad2fe9289281e1
sad126 <frhatsd@gmail.com>
2024-10-29 15:57:15 +0300

    edit2

---
+++ Assets/scripts/Ex_1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex_1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int count = 0
+        while (count < 20){
+            int num = new Random(1, 21);
+            if(num == 5){
+                continue;
+            }
+            Debug.Log(num);
+            if(num == 15){
+                break;
+            }
+            count++;
+        }
+    }
+
+    // Update is called once per frame
+   
+}
